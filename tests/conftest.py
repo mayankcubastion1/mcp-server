3efc9fd927acc2f74c6fa404618bc9b9136cc63b
Mayank Sharma <mayank.sharma1@cubastion.com>
2025-08-06 12:52:17 +0530

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,3 +2,4 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src")))
+os.environ.setdefault("HRMS_API_BASE_URL", "https://devxnet2api.cubastion.net/api/v2")
