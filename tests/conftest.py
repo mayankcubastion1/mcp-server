1f1a1242166ca069bbfa87e2ac1e1cfa650dc8b5
SHMAYAN <mayank.sharma1@cubastion.com>
2025-08-26 13:51:06 +0530

    chore: remove tests

--- tests/conftest.py
+++
@@ -1,5 +0,0 @@
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src")))
-os.environ.setdefault("HRMS_API_BASE_URL", "https://devxnet2api.cubastion.net/api/v2")
